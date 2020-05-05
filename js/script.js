a56ad13744633e9a3fc9eabb342bcfebee8fb47a
Carolina <n.eugenia.c@gmail.com>
2020-05-05 14:00:09 +0300

    Add all filles

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.owl-carousel').owlCarousel({
+        autoplay: true,
+        autoplayHoverPause: true,
+        items: 3,
+        nav: true,
+        dots: true,
+        loop: true,
+        dots: 3
+      });
