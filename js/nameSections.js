20d9e7fe3b59d7b51471c57da5ac8bee27f67f6c
Carolina <n.eugenia.c@gmail.com>
2020-05-11 08:18:11 +0300

    Added JavaScript file for name sections in the page

---
+++ js/nameSections.js
@@ -0,0 +1,4 @@
+document.getElementById('name-page').innerHTML = "CARA ANON";
+document.getElementById('name-section1').innerHTML = "EARRINGS";
+document.getElementById('name-section2').innerHTML = "BRACELETS";
+document.getElementById('name-section3').innerHTML = "NECKLACES"; 
